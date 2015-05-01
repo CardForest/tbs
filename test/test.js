6d1b12d1bd4b311390b151394677a83c0e7513b1
Amit Portnoy <amit.portnoy@gmail.com>
2015-05-01 17:54:21 +0300

    refactor tbs client - add basic change applying

--- test/test.js
+++ test/test.js
@@ -17,7 +17,7 @@ var gameTypes = {
 var localGameFactory = require('../lib/localGameFactory');
 
 describe('tbs module', function () {
-  it('inline local game', function (done) {
+  it.skip('inline local game', function (done) {
     var service = new tbs.Service(
       new tbs.inMem.GameRoomService(),
       new tbs.inMem.GameTypeService(gameTypes),
@@ -42,13 +42,30 @@ describe('tbs module', function () {
     });
   });
 
-  it('localGameFactory', function (done) {
+  it.skip('localGameFactory', function (done) {
     localGameFactory(gameTypes['test-game']).spread(function (client1, client2) {
-      assert.equal(client1.credentials.userId, '0');
-      assert.equal(client2.credentials.userId, '1');
+      assert.equal(client1.credentials.userId, '1');
+      assert.equal(client2.credentials.userId, '2');
 
       assert.equal(client1.room.game, 'test-game-instance');
       assert.equal(client2.room.game, 'test-game-instance');
+
+      done();
+    });
+  });
+
+  it('localGameFactory with master-class', function (done) {
+    localGameFactory(require('./whist')).spread(function (client1, client2, client3, client4) {
+      assert.equal(client1.credentials.userId, '1');
+      assert.equal(client2.credentials.userId, '2');
+      assert.equal(client3.credentials.userId, '3');
+      assert.equal(client4.credentials.userId, '4');
+
+
+      client1.room.game.putDownCard(3);
+      client2.room.game.putDownCard(0);
+      //assert.equal(client1.room.game, 'test-game-instance');
+      //assert.equal(client2.room.game, 'test-game-instance');
 
       done();
     });
---
+++ test/whist.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var path = require('path');
+var M = require('master-class');
+
+function shuffle(array) {
+  var count = array.length,
+    rand,
+    temp;
+  while (count) {
+    rand = Math.random() * count-- | 0;
+    temp = array[count];
+    array[count] = array[rand];
+    array[rand] = temp;
+  }
+}
+
+module.exports = {
+  id: path.basename(__filename, '.js'),
+  numOfPlayers: 4,
+  MasterClass: M({
+    props: {
+      currentPlayerIdx: M.Number(),
+      players: M.Array({
+        defaultLength: 2,
+        elem: M.Object({
+          props: {
+            cards: M.Array({
+              defaultLength: 13,
+              elem: M.Number({scope: 'player'})
+            }),
+            down: M.Number({initialValue: -1})
+          }
+        })
+      }),
+      putDownCard: M.Mutator({
+        fn: function (requesterIdx, cardIdx) {
+          this.players[requesterIdx].down = this.players[requesterIdx].cards[cardIdx];
+          this.players[requesterIdx].cards[cardIdx] = -1;
+        }
+      }),
+      onStart: M.Mutator({
+        fn: function () {
+          var i, deck = [];
+          for (i = 0; i < 52; i++) {
+            deck.push(i);
+          }
+          shuffle(deck);
+          for (i = 0; i < 13; i++) {
+            this.players[0].cards[i] = deck[i];
+            this.players[1].cards[i] = deck[i + 13];
+            this.players[2].cards[i] = deck[i + 26];
+            this.players[3].cards[i] = deck[i + 39];
+          }
+        }
+      }),
+    }
+  })
+};
+
