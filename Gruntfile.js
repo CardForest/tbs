d17b250a5a14b901cdd2fc7dd303ddb7cffa9006
Amit Portnoy <amit.portnoy@gmail.com>
2015-05-02 16:12:21 +0300

    added primus trnasport

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
         src: ['Gruntfile.js']
       },
       js: {
-        src: ['*.js']
+        src: ['*.js', 'lib/**/*.js', '!lib/transport/primus/generatedPrimusClient.js']
       },
       test: {
         src: ['test/**/*.js']
